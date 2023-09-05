2176f143271dd39b6f0717128f21051a1e5ac5e8
BreakfastSandwich <143359791+BreakfastSandwich@users.noreply.github.com>
2023-09-05 09:56:38 -0400

    morning commit

--- assets.css/script.js
+++ assets.css/script.js
@@ -1,6 +1,7 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarlinks = document.getElementsByClassName('navbar-links')[0]
+console.log("inside script file");
 
 toggleButton.addEventListener('click', () => {
     navbarlinks.classList.toggle('.active')
-})
+});
