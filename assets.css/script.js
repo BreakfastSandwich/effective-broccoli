8090a09370bd652968b3d73f4cafa847435ab7f7
BreakfastSandwich <143359791+BreakfastSandwich@users.noreply.github.com>
2023-09-06 22:41:34 -0400

    ready to update readme & publish

--- assets.css/script.js
+++ assets.css/script.js
@@ -1,9 +1,11 @@
 const toggleButton = document.getElementsByClassName("toggle-button")[0]
 const navbarLinks = document.getElementsByClassName("navbar-links")[0]
-// console.log(navbar-Links);
+
 
 toggleButton.addEventListener("click", () => {
     navbarLinks.classList.toggle("active")
     navbarLinks.classList.toggle("hide")
-    console.log("taco")
+    
+
+   
 });
